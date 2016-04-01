903d24044eec8dd9d5e801ecd7ef42e6621704fb
Sergio <sata@ic.ufal.br>
2016-04-01 12:35:23 -0300

    global.h incluida, constantes sendo usadas

--- avl_vs_bst/self_unitary_tests.c
+++ avl_vs_bst/self_unitary_tests.c
@@ -1,44 +1,56 @@
+/* Este programa realiza testes unitários nas funções do projeto avl_vs_bst */
+
+
 #include <stdio.h>
-#include "randomize.h"
+#include "randomize.h" /* biblioteca de funções de randomização */
+#include "global.h" /* definição de constantes globais para o projeto */
+
+#define TESTING_NUMBER (17) /* inteiro usado para testar funções */
+#define MIN_ARRAY_SIZE (1) /* tamanho mínimo para um array */
 
 /* Funções da biblioteca randomize.h */
-int test_random_interger();
-int test_sort_a_number();
+int test_random_interger(); /* testa a função random_interger */
+int test_sort_a_number(); /* testa a função sort_a_number */
 
 
 /* Para cada função testada o prograva deve exibir o nome da função e OK se
- * ela passar no teste ou FAIL caso o resultado não seja o esperado. */
-int main () {
-    printf("random_interger function\t\t");
+ * ela passar no teste ou FAIL caso o resultado não seja o esperado. 
+ */
+void main () {
     
-    if (test_random_interger())
+    /* imprimir OK se a função random_interger estiver funcionando */
+    printf("random_interger function\t\t");
+    if (test_random_interger() != FALSE)
         printf("OK\n");
     else
         printf("FAIL\n");
 
+
+    /* imprimir OK se a função sort_a_number estiver funcionando */
     printf("sort_a_number function\t\t");
-    if (test_sort_a_number())
+    if (test_sort_a_number() != FALSE)
         printf("OK\n");
     else
         printf("FAIL\n");
 
-    return (0);
 }
 
 /* Retorna 1 caso a função random_interger esteja funcionando corretamente
- * e 0 caso contrário */
+ * e 0 caso contrário
+ */
 int test_random_interger(){
-    if (random_interger(17,17)==17)
-        return (1);
-    return (0);
+    if (random_interger(TESTING_NUMBER, TESTING_NUMBER) == TESTING_NUMBER)
+        return (TRUE);
+    return (FALSE);
 }
 
 /* Retorna 1 caso a função sort_a_number esteja funcionando corretamente 
- * e 0 caso contrário. */
+ * e 0 caso contrário.
+ */
 int test_sort_a_number(){
-    int array[1];
-    array[0] = 17;
-    if (sort_a_number(array,1)==17)
-        return (1);
-    return (0);
+    int array[MIN_ARRAY_SIZE];
+    array[ZERO] = TESTING_NUMBER;
+    if (sort_a_number(array, MIN_ARRAY_SIZE) == TESTING_NUMBER)
+        return (TRUE);
+    return (FALSE);
 }
