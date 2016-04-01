72449338acbe4f41689b8c9a218657176c74c572
Sergio <sata@ic.ufal.br>
2016-04-01 19:08:19 -0300

    correção de erros nos resultados das funções

--- avl_vs_bst/arguments.h
+++ avl_vs_bst/arguments.h
@@ -3,8 +3,7 @@
 
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> /* atoi */
 #include "global.h" /* constantes e funções globais */
 
 
@@ -46,51 +45,54 @@ int char_to_int(char character) {
  * Retorna o valor inteiro o argumento desejado, ou ERROR caso o valor não
  * seja encontrado */
 int get_argument_value(int args_count, char *args[], char arg) {
-	int i; /* indíce para laço */
+
 	int index_arg_value_str = ZERO; /* indíce para adicionar valor à string */
 	int arg_value; /* receberá o inteiro do valor do argumento */
 	char arg_value_str[MAX_VALUE_LENGTH]; /* string para valor do argumento */
+	char char_pos = 1; /* receberá posição do caracter do argumento no array*/
 
 	/* receberá posição onde o argumento se encontra no array, ou FALSE */
 	int arg_pos = argument_is_set(args_count, args, arg); 
 
 	/* caso não exista o argumento desejado no array de argumentos: ERROR */
-	if (arg_pos == FALSE)
-		return ERROR; /* o argumento não está no array de argumentos */ 
-	
-	/* verificar argumentos em busca do argumento solicitado */
-	if (args[arg_pos][ZERO] == ARG_SYMBOL && args[i][1] == arg) {
-		
-		/* salvar o valor em uma string, caso esteja concatenado */
-		while (char_to_int(args[i][(index_arg_value_str+2)]) != ERROR) {
-			arg_value_str[index_arg_value_str] =
-				args[i][(index_arg_value_str+2)];
-			index_arg_value_str++;
-		}
-
-		/* se o valor do argumento não foi encontrado concatenado ao argumen-
-		 * to, o valor deve estar no próximo argumento */
-		if (index_arg_value_str == ZERO) {
-			/* salvar o valor em uma string */
-			while (char_to_int(args[(arg_pos+1)][index_arg_value_str]) !=
-				ERROR) {
-
-				arg_value_str[index_arg_value_str] = 
-					args[(arg_pos+1)][index_arg_value_str];
-				index_arg_value_str++;
-			}
-		}
-
-		/* se nenhum valor foi obtido, houve erro ao passar os argumentos */
-		if (index_arg_value_str == ZERO)
-			return ERROR;
-
-		/* finalizar a string com o valor obtido */
-		arg_value_str[index_arg_value_str] = NULL_CHAR;
-		arg_value = atoi(arg_value_str); /* inteiro do valor obtido */
-
-		return arg_value; /* retornar o valor obtido como inteiro */
+	if (arg_pos == FALSE){
+		return ERROR; /* o argumento não está no array de argumentos */
 	}
 
-	return ERROR; /* o argumento não foi encontrado */
+	/* encontrar a posição do caracter do argumento desejado */
+	while ( args[arg_pos][char_pos] != arg) {
+		char_pos++;
+	}
+		
+	/* salvar o valor em uma string, caso esteja concatenado */
+	while (char_to_int(args[arg_pos][(index_arg_value_str+(char_pos+1))]) !=
+		ERROR) {
+
+		arg_value_str[index_arg_value_str] =
+			args[arg_pos][(index_arg_value_str+(char_pos+1))];
+		index_arg_value_str++;
+
+	}
+
+	/* se o valor do argumento não foi encontrado concatenado ao argumen-
+	 * to, o valor deve estar no próximo argumento */
+	if (index_arg_value_str == ZERO) {
+		/* salvar o valor em uma string */
+		while (char_to_int(args[(arg_pos+1)][index_arg_value_str]) != ERROR) {
+			arg_value_str[index_arg_value_str] = 
+				args[(arg_pos+1)][index_arg_value_str];
+			index_arg_value_str++;
+		}
+	}
+
+	/* se nenhum valor foi obtido, houve erro ao passar os argumentos */
+	if (index_arg_value_str == ZERO)
+		return ERROR;
+
+	/* finalizar a string com o valor obtido */
+	arg_value_str[index_arg_value_str] = NULL_CHAR;
+	arg_value = atoi(arg_value_str); /* inteiro do valor obtido */
+
+	return arg_value; /* retornar o valor obtido como inteiro */
+
 }
