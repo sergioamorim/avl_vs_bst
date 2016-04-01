05bec416ba6dd403efea076b89a292cc36a3631e
Sergio <sata@ic.ufal.br>
2016-04-01 12:34:18 -0300

    constantes zero, error e tipo bool

---
+++ avl_vs_bst/global.h
@@ -0,0 +1,15 @@
+/* Tipos abstratos de dados e constantes que podem ser utilizadas em qualquer
+ * parte do projeto.
+ */
+
+
+/* Definição do tipo booleano, para facilitar a leitura do código */
+typedef int bool;
+#define FALSE (0)
+#define TRUE (1)
+
+/* Constante ZERO, para facilitar a leitura do código */
+#define ZERO (0)
+
+/* Constante de erro padrão */
+#define ERROR (-1)
